4dc58530b62d245452956f733d8af30743334e45
Bhoomika <bhoomikahm18@gmail.com>
2023-11-11 14:57:33 +0530

    Adding MUI Date Range Picker

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Chip, Divider } from '@mui/material';
 import CalenderComp from './components/CalenderComp';
 import DateRangeComp from './components/DateRangeComp';
 import DateRangePickerComp from './components/DateRangePickerComp';
+import MUIDateRange from './components/MUIDateRange';
 
 function App() {
   return (
@@ -11,7 +12,13 @@ function App() {
     <CalenderComp />
 
     <Divider><Chip label="Date Range"/></Divider>
+    <DateRangeComp />
+
+    <Divider><Chip label="Date Range Picker"/></Divider>
     <DateRangePickerComp />
+
+    <Divider><Chip label="MUI Date Range Picker"/></Divider>
+    <MUIDateRange />
     </>
   );
 }
